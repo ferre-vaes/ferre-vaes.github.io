955264b8b885c523f393f923159ee95ffa02fee0
ferre-vaes <devferre@gmail.com>
2020-04-14 22:05:57 +0200

    added pages for activities

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: true,
     bigSectionsDestination:top,
+    anchors: ['presentation', 'tweakers', 'brugge', 'berlijn', 'IBM'],
+    navigationTooltips: ['Voorstelling', 'Tweakers', 'Brugge', 'Berlijn', 'IBM']
 });
